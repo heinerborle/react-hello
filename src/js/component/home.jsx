c6e87dd9e5a471148713efcd53a165a4ae5d10e4
heinerborle <heinerborle@gmail.com>
2022-04-07 20:50:38 +0000

    Fully working website with all the components required in React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<Jumbotron />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-3 col-md-3 col-sm-12">
+						<Card />
+					</div>
+					<div className="col-lg-3 col-md-3 col-sm-12">
+						<Card />
+					</div>
+					<div className="col-lg-3 col-md-3 col-sm-12">
+						<Card />
+					</div>
+					<div className="col-lg-3 col-md-3 col-sm-12">
+						<Card />
+					</div>
+				</div>
+				<div className="row">
+					<Footer />
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Jumbotron = () => {
+	return (
+		<main className="container">
+			<div className="bg-light rounded mt-5 mb-5">
+				<div className="container pb-5 pt-5">
+					<h1>A Warm Welcome!</h1>
+					<p className="lead">
+						Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit. Voluptatem, ex qui, voluptatibus ducimus atque
+						maxime ab, autem repellendus laboriosam eaque ut
+						mollitia saepe natus quam quisquam omnis. Rerum,
+						deleniti omnis?
+					</p>
+					<a
+						className="btn btn-lg btn-primary"
+						href="#"
+						role="button">
+						Call to action
+					</a>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default Jumbotron;
